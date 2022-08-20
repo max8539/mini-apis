cb38f66ba70ce15baa765a2dfb4bb7c46141a920
max8539 <89776086+max8539@users.noreply.github.com>
2022-08-20 22:50:03 +1000

    Finish quotemaster API routes

--- quotemaster/quotemaster.js
+++ quotemaster/quotemaster.js
@@ -1,7 +1,27 @@
 import fs from "fs";
-import path from "path";
-const QUOTES_JSON = "./quotemaster/quotes.json"
+import crypto from "crypto";
 
+const QUOTES_JSON = "./quotemaster/quotes.json";
+const QUOTES_DEFAULT_JSON = "./quotemaster/quotes-default.json";
+
+const HASHEDPASS = "dX2+ujAOKQmKLSaOE7DXKRcz832YgaJupwXe0Q5Sqnw=";
+
+// Custom errors
+export class idError extends Error{};
+export class quoteError extends Error{};
+export class nameError extends Error{};
+export class passError extends Error{};
+
+// Initialisation - creates quotes.json if it doesn't exist
+// (required for first run after git clone)
+export function init () {
+    if (!fs.existsSync(QUOTES_JSON)) {
+        let data = JSON.parse(fs.readFileSync(QUOTES_DEFAULT_JSON));
+        fs.writeFileSync(QUOTES_JSON, JSON.stringify(data))
+    }
+}
+
+// /quotemaster/random
 export function randomQuote () {
     const DATA = JSON.parse(fs.readFileSync(QUOTES_JSON));
     let num = DATA.quotes.length;
@@ -9,10 +29,57 @@ export function randomQuote () {
     return DATA.quotes[index];
 }
 
+// /quotemaster/popular
 export function popularQuote () {
     const DATA = JSON.parse(fs.readFileSync(QUOTES_JSON));
     let popular = DATA.quotes.filter(item => item.likes >= DATA.maxLikes * 0.75);
     let num = popular.length;
     let index = Math.floor(Math.random() * num);
     return popular[index];
+}
+
+// /quotemaster/like
+export function likeQuote (id) {
+    const DATA = JSON.parse(fs.readFileSync(QUOTES_JSON));
+    console.log(id);
+    if (id >= DATA.quotes.length) {
+        throw new idError;
+    }
+    DATA.quotes[id].likes += 1;
+    if (DATA.quotes[id].likes > DATA.maxLikes) {
+        DATA.maxLikes += 1;
+    }
+    fs.writeFileSync(QUOTES_JSON, JSON.stringify(DATA));
+}
+
+// /quotemaster/new
+export function newQuote (quote, name) {
+    if (typeof(quote) != "string") {
+        throw new quoteError;
+    } else if (quote.length < 1 || quote.length > 400) {
+        throw new quoteError;
+    } else if (typeof(name) != "string") {
+        throw new nameError;
+    } else if (name.length < 1 || name.length > 40) {
+        throw new nameError;
+    }
+    const DATA = JSON.parse(fs.readFileSync(QUOTES_JSON));
+    DATA.quotes.push({
+        "id": DATA.quotes.length,
+        "quote": quote,
+        "name": name,
+        "likes": 0
+    });
+}
+
+// /quotemaster/reset
+export function resetQuote (pass) {
+    const hasher = crypto.createHash("sha256");
+    hasher.update(pass);
+    let hash = hasher.digest("base64");
+    if (hash != HASHEDPASS) {
+        throw new passError;
+    }
+    const DEFAULT = JSON.parse(fs.readFileSync(QUOTES_DEFAULT_JSON));
+    fs.writeFileSync(QUOTES_JSON, JSON.stringify(DEFAULT));
 }
--- run.js
+++ run.js
@@ -1,17 +1,68 @@
 import express from "express";
+import cors from "cors";
 import * as quotemaster from "./quotemaster/quotemaster.js";
+import { appendFile } from "fs";
 
 const APP = express();
+APP.use(cors());
+APP.use(express.json());
+
+// API initialisation functions (where required)
+quotemaster.init();
 
 // quotemaster API interface
 APP.get("/quotemaster/random", function (req, res) {
     const QUOTE = quotemaster.randomQuote();
     res.json(QUOTE);
-})
+});
 APP.get("/quotemaster/popular", function (req, res) {
     const QUOTE = quotemaster.popularQuote();
     res.json(QUOTE);
-})
+});
+APP.post("/quotemaster/like", function (req, res) {
+    try {
+        quotemaster.likeQuote(req.body.id);
+        res.status(200).end();
+    } catch (err) {
+        if (err instanceof quotemaster.idError) {
+            res.status(400).json({
+                "errorMessage": "Invalid quote ID"
+            });
+        } else {
+            throw(err);
+        }
+    }
+});
+APP.post("/quotemaster/new", function (req, res) {
+    try {
+        quotemaster.newQuote(req.body.quote, req.body.name);
+        res.status(200).end();
+    } catch (err) {
+        if (err instanceof quotemaster.quoteError) {
+            res.status(400).json({
+                "errorMessage": "Quote must be between 1 and 400 characters long"
+            });
+        } else if (err instanceof quotemaster.nameError) {
+            res.status(400).json({
+                "errorMessage": "Name must be between 1 and 40 characters long"
+            });
+        } else {
+            throw(err);
+        }
+    }
+});
+APP.post("/quotemaster/reset", function (req, res) {
+    try {
+        quotemaster.resetQuote(req.body.pass);
+        res.status(200).end();
+    } catch (err) {
+        if (err instanceof quotemaster.passError) {
+            res.status(403).json({
+                "errorMessage": "Invalid password"
+            });
+        }
+    }
+});
 
 
 
