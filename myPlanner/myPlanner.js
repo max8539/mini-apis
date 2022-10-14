9f8db172fae4ecd8982fe6f31b199a166d66f683
max8539 <89776086+max8539@users.noreply.github.com>
2022-10-15 01:30:01 +1100

    MyPlanner: clear existing logic

--- myPlanner/myPlanner.js
+++ myPlanner/myPlanner.js
@@ -14,63 +14,27 @@ export class BadPassError extends Error{};
 export class InvalidLoginError extends Error{};
 
 function loadData () {
-    return JSON.parse(fs.readFileSync(DATA_JSON));
+    
 }
 
 function saveData (data) {
-    fs.writeFileSync(DATA_JSON, JSON.stringify(data));
+    
 }
 
 function passwordHash (password) {
-    let hasher = crypto.createHash("hs256");
-    hasher.update(password);
-    return hasher.digest("base64");
+    
 }
 
 export function init () {
-    if (!fs.existsSync(DATA_JSON)) {
-        let data = JSON.parse(fs.readFileSync(DATA_DEFAULT_JSON));
-        fs.writeFileSync(DATA_JSON, JSON.stringify(data))
-    }
+    
 }
 
 export function checkToken (token) {
-    let tokenData;
-    try {
-        tokenData = jsonwebtoken.verify(token, process.env.TOKEN_KEY,{algorithms:["HS256"]})
-    } catch (err) {
-        throw new InvalidTokenError;
-    }
-    let userExists = false;
-    let validDate;
-
-    const DATA = loadData();
-    for (user of DATA.users) {
-        if (user.uname == tokenData.uname) {
-            userExists = true;
-            validDate = (tokenData.sysLoginTime >= user.earliestLoginTime);
-        }
-    }
-
-    if (userExists && validDate) {
-        return {
-            "uname": tokenData.uname,
-        }
-    } else {
-        throw new InvalidTokenError;
-    }
     
 }
 
 export function register (uname, email, pass) {
-    let data = loadData();
-    let hashedPass = passwordHash(pass);
-    let newUser = {
-        uname: uname,
-        email: email,
-        pass: hashedPass
-    }
-
+    
 }
 
 export function login (uname, pass) {
